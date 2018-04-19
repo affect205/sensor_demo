5d0e1b56cb7b01d5dbe0b35083fd813c00164712
Alex <alexbalu-alpha7@mail.ru>
2018-04-19 17:54:27 +0300

    added first commit

---
+++ src/main/java/com/alexside/sensor_demo/SensorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.alexside.sensor_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexside/sensor_demo/SensorDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexside.sensor_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SensorDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
